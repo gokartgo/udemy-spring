6029d69c79bc9230034bc77fec34b2eadc2c9373
gokartgo <gokart.007@hotmail.com>
2022-01-02 14:25:53 +0700

    finish spring boot application properties

--- mycoolapp/src/main/kotlin/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.kt
+++ mycoolapp/src/main/kotlin/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.kt
@@ -1,11 +1,23 @@
 package com.luv2code.springboot.demo.mycoolapp.rest
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
 class FunRestController {
+    @Value("\${coach.name}")
+    private val coachName: String ?= null
+
+    @Value("\${team.name}")
+    private val teamName: String ?= null
+
+    @GetMapping("/teaminfo")
+    public fun getTeamInfo(): String {
+        return "Coach: ${coachName}, Team: ${teamName}"
+    }
+
     @GetMapping("/")
     public fun sayHello(): String {
         return "Hello World! Time on server is ${LocalDateTime.now()}"
